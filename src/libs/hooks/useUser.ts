965168c6d9cb9215c3ea51869057b21919c748be
idbd <idbd.young@gmail.com>
2022-06-02 11:29:12 +0900

    FIX 인증방식 변경

--- src/libs/hooks/useUser.ts
+++ src/libs/hooks/useUser.ts
@@ -13,7 +13,13 @@ const fetcher: Fetcher<{ user?: User }> = async (url: string) => {
   if (!cookieClient.get(process.env.REACT_APP_TOKEN_NAME)) {
     return {};
   }
-  const res = await fetch(url, { credentials: 'include' });
+  const res = await fetch(url, {
+    credentials: 'include',
+    headers: {
+      Authorization: `Bearer ${cookieClient.get(process.env.REACT_APP_TOKEN_NAME)}`,
+    },
+  });
+
   if (!res.ok) {
     throw new Error('유효한 토큰이 아닙니다. 다시 로그인 해 주세요');
   }
