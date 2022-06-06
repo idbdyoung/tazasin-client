eea96ae2d9b70ae98a9f0e44cf95932421732613
idbd <idbd.young@gmail.com>
2022-06-06 14:17:51 +0900

    FEAT turn 서버 연결

--- src/modules/game/Peer.ts
+++ src/modules/game/Peer.ts
@@ -111,6 +111,11 @@ class Peer {
     const pc = new RTCPeerConnection({
       iceServers: [
         {
+          urls: ['turn:tazasin.com:3478?transport=tcp'],
+          username: process.env.REACT_APP_TURN_SERVER_USER_NAME,
+          credential: process.env.REACT_APP_TURN_SERVER_USER_PASSWD,
+        },
+        {
           urls: ['stun:stun.l.google.com:19302', 'stun:stun1.l.google.com:19302'],
         },
       ],
